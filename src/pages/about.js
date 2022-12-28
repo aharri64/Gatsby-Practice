15c7f0e6b5afe1c47cde92b6488daea9cf4c0a2f
Amir Harrison <76721355+aharri64@users.noreply.github.com>
2022-12-28 09:02:11 -0700

    correct commit

---
+++ src/pages/about.js
@@ -0,0 +1,30 @@
+import React from "react";
+import Layout from "../components/Layout";
+
+export default function About() {
+    return (
+        <Layout>
+            <div>
+                <h1>About Page</h1>
+                <p>
+                    Lorem ipsum dolor sit amet consectetur adipisicing elit.
+                    Debitis, vero similique. Ipsa, repellendus autem nam
+                    corporis ex enim blanditiis optio neque cum cupiditate ab
+                    molestias nihil, dolores esse earum necessitatibus.
+                </p>
+                <p>
+                    Lorem ipsum dolor sit amet consectetur adipisicing elit.
+                    Debitis, vero similique. Ipsa, repellendus autem nam
+                    corporis ex enim blanditiis optio neque cum cupiditate ab
+                    molestias nihil, dolores esse earum necessitatibus.
+                </p>
+                <p>
+                    Lorem ipsum dolor sit amet consectetur adipisicing elit.
+                    Debitis, vero similique. Ipsa, repellendus autem nam
+                    corporis ex enim blanditiis optio neque cum cupiditate ab
+                    molestias nihil, dolores esse earum necessitatibus.
+                </p>
+            </div>
+        </Layout>
+    );
+}
